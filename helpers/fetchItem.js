c919f26189caed2ef5e6714481b486c9fb9f0540
Vitor <vitor.belarmino@hotmail.com>
2022-02-03 11:51:03 -0300

    implementa a funçao fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,14 @@
-const fetchItem = () => {
 
+const fetchItem = async (item) => {
+try {
+  const url = `https://api.mercadolibre.com/items/${item}`;
+  const response = await fetch(url);
+  const data = response.json();
+  return data;
+} catch (error) {
+  return error;
+}
 };
-
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
