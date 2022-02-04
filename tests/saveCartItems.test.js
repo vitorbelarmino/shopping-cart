435909f67831ec4ff0fabb72ee56cb705f86c9c8
Vitor <vitor.belarmino@hotmail.com>
2022-02-04 16:42:03 -0300

    adiciona getSavedCartTims, funçao e testes

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -11,5 +11,5 @@ describe('4 - Teste a função saveCartItems', () => {
   it('ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
     const result = saveCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
-  })
+  });
 });
